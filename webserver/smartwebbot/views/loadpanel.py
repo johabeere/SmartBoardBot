c39769a938112f0610a89592380efa3cb25cb285
Benedikt Döring <benediktdoering@bdmuvt.de>
2022-06-14 14:20:30 +0200

    Panel: Show last picture

--- webserver/smartwebbot/views/loadpanel.py
+++ webserver/smartwebbot/views/loadpanel.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 from smartwebbot.models.Media import Media
 
@@ -14,5 +16,16 @@ def loadpanel(request):
     elif target == 'my-uploads':
         files = Media.objects.all()
         return render(request, 'panels/uploads/my-uploads.html', {'files': files})
+    elif target == 'show-camera':
+        img_dir = os.getcwd() + "/smartwebbot/static/img/photos/"
+
+        filename = "not_found"
+        for i in range(0,100000):
+            if not os.path.exists(img_dir+"image"+str(i)+".jpg"):
+                break
+            else:
+                filename = "image"+str(i)
+
+        return render(request, 'panels/camera/show-camera.html', {'img_url': "/static/img/photos/"+filename+".jpg"})
     else:
         return render(request, 'panels/error.html')
