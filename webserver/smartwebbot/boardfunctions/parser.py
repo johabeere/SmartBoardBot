4a1cfa42c76f49259f38d1a37ba2103d9b48ab4d
Benedikt Döring <benediktdoering@bdmuvt.de>
2022-06-17 12:29:57 +0200

    Replaced svg to gcode parser

--- webserver/smartwebbot/boardfunctions/parser.py
+++ webserver/smartwebbot/boardfunctions/parser.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import subprocess
 
 import svg_to_gcode
 from svg_to_gcode.svg_parser import parse_file
@@ -12,13 +13,18 @@ def parse(source, target):
 
     # Instantiate a compiler, specifying the interface type and the speed at which the tool should move. pass_depth controls
     # how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
-    gcode_compiler = Compiler(interfaces.Gcode, movement_speed=8000, cutting_speed=2000, pass_depth=0)
+    #gcode_compiler = Compiler(interfaces.Gcode, movement_speed=1000, cutting_speed=300, pass_depth=0)
+    #gcode_compiler = Compiler(interfaces.Gcode, movement_speed=8000, cutting_speed=2000, pass_depth=0)
 
-    curves = parse_file(source) # Parse an svg file into geometric curves
+    #curves = parse_file(source) # Parse an svg file into geometric curves
 
-    gcode_compiler.append_curves(curves)
-    gcode_compiler.unit = "mm"
-    gcode_compiler.compile_to_file(target, passes=1)
+    #gcode_compiler.append_curves(curves)
+    #gcode_compiler.unit = "mm"
+    #gcode_compiler.compile_to_file(target, passes=1)
+
+    output = subprocess.check_output(["python2", "svg2gcode/svg2gcode.py", source, target])
+    logging.info("Got return: \n" + str(output))
+    #generate_gcode(source, target)
     logging.info("Finished parsing.")
 
 def getNextSourceIndex():
