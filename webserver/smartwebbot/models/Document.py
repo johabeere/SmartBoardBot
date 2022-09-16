56f33a700782a7249ae13f84976e78819d8df681
johannes <johannes.t.schraml@gmail.com>
2022-09-16 15:24:51 +0200

    added Gcode functionality to Document Model 🥵

--- webserver/smartwebbot/models/Document.py
+++ webserver/smartwebbot/models/Document.py
@@ -4,13 +4,16 @@ from smartwebbot import constants
 
 class Document(Media):
     @classmethod
-    def create(cls, user, title, data, is_upload=True, *args, **kwargs):
+    def create(cls, user, title, data, is_upload=False, doctype, *args, **kwargs):
         document = cls(None, title)
-        document.fileType = constants.PDF
-        if is_upload:
-            document.type = constants.UPLOAD
-        else:
-            document.type = constants.SCAN
+        if doctype == "PDF":
+            document.fileType = constants.PDF
+            if is_upload:
+                document.type = constants.UPLOAD
+            else:
+                document.type = constants.SCAN
+        elif doctype="GCODE":
+            document.filetype = constants.GCODE
         document.user = user
         document.title = title
         document.data = data
